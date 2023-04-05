ab0e59ab072c7ed535674cb74cb62a5d0495d9ce
Santiago Martinez <santiago.mbal@gmail.com>
2023-04-05 13:19:06 -0600

    Added a test for MultiIntervalRecordingManager

--- tests/test_recording_managers.py
+++ tests/test_recording_managers.py
@@ -1,21 +1,107 @@
 """Test the recording managers."""
 
-from datetime import time, timezone
+import datetime
 
-from acoupi.recording_managers import Interval, IntervalRecordingManager
+from acoupi.recording_managers import (
+    Interval,
+    IntervalRecordingManager,
+    MultiIntervalRecordingManager,
+)
 
 
 def test_interval_recording_manager_is_inclusive():
     """Test the interval recording manager."""
-    interval = Interval(start=time(10, 0), end=time(11, 0))
+    interval = Interval(
+        start=datetime.time(10, 0),
+        end=datetime.time(11, 0),
+    )
 
     recording_manager = IntervalRecordingManager(
         interval,
-        timezone=timezone.utc,
+        timezone=datetime.timezone.utc,
     )
 
-    assert recording_manager.should_record(time(9, 59)) is False
-    assert recording_manager.should_record(time(10, 0)) is True
-    assert recording_manager.should_record(time(10, 30)) is True
-    assert recording_manager.should_record(time(11, 0)) is True
-    assert recording_manager.should_record(time(11, 1)) is False
+    now = datetime.datetime.now().replace(
+        hour=0,
+        minute=0,
+        second=0,
+        microsecond=0,
+    )
+
+    assert (
+        recording_manager.should_record(now.replace(hour=9, minute=59))
+        is False
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=10, minute=0)) is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=10, minute=30))
+        is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=11, minute=0)) is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=11, minute=1))
+        is False
+    )
+
+
+def test_multiple_interval_recording_manager():
+    """Test the multiple interval recording manager."""
+    intervals = [
+        Interval(
+            start=datetime.time(10, 0),
+            end=datetime.time(11, 0),
+        ),
+        Interval(
+            start=datetime.time(12, 0),
+            end=datetime.time(13, 0),
+        ),
+    ]
+
+    recording_manager = MultiIntervalRecordingManager(
+        intervals,
+        timezone=datetime.timezone.utc,
+    )
+
+    now = datetime.datetime.now().replace(
+        hour=0,
+        minute=0,
+        second=0,
+        microsecond=0,
+    )
+
+    assert (
+        recording_manager.should_record(now.replace(hour=9, minute=59))
+        is False
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=10, minute=0)) is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=10, minute=30))
+        is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=11, minute=0)) is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=11, minute=1))
+        is False
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=12, minute=0)) is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=12, minute=30))
+        is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=13, minute=0)) is True
+    )
+    assert (
+        recording_manager.should_record(now.replace(hour=13, minute=1))
+        is False
+    )
