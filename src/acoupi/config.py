cdf1fe3c94d215215b23812204df7f168117cc05
audevuilli <aude.vuilli@gmail.com>
2023-04-10 13:01:03 +0100

    update config.py

--- src/acoupi/config.py
+++ src/acoupi/config.py
@@ -7,7 +7,8 @@ LONGITUDE = -0.0099
 """Default audio recording parameters."""
 DEFAULT_SAMPLE_RATE = 192000
 DEFAULT_AUDIO_CHANNELS = 1
-DEFAULT_CHUNK_SIZE = 1024
+DEFAULT_CHUNK_SIZE = 2048
+# DEFAULT_CHUNK_SIZE = 1024
 
 """Default audio recording lenght in seconds."""
 DEFAULT_RECORDING_DURATION = 3
