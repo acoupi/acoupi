45f33dbdc0520e8400af9ce56c3a8cf4f5ff1a0f
Santiago Martinez <santiago.mbal@gmail.com>
2023-04-15 16:31:48 -0600

    Better templates documentation

--- src/acoupi/templates.py
+++ src/acoupi/templates.py
@@ -1,4 +1,21 @@
-"""Process templates for Acoupi."""
+"""Process templates for Acoupi.
+
+Acoupi offers a collection of process templates to assist in the creation of
+recording, detecting, and data sending processes. While Acoupi includes a
+variety of components to construct these processes, users may prefer to use
+their own components. By utilizing the provided templates, users can ensure
+that their custom processes integrate with Acoupi and adhere to standardized
+building practices. The use of templates also allows for effortless
+customization of processes.
+
+The templates provided take the form of functions that return a function that
+can be used to start a process. Each template takes a set of arguments that
+are used to construct the process. The arguments are Acoupi components
+of the appropriate type, such as a message store, messenger, model, etc.
+Any object that implements the appropriate interface can be used as an
+argument. This allows users to use out-of-the-box components or components
+that they have created themselves.
+"""
 from typing import Callable
 
 from acoupi import types
@@ -18,6 +35,10 @@ def build_send_data_process(
     Use this function to build a process that will send data to a remote server
     using your preferred messenger and message store. This function will return
     a function that can be used to start the process.
+
+    The built process will send unsynced deployments, recordings, and detections
+    to the remote server. The process will then store the response from the
+    remote server, so that the data can be marked as synced.
 
     Args:
         message_store: The message store to use. The message store
