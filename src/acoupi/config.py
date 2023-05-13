9e91309ae317e3823b9edb383c75b0c5df621b66
Aude <47717187+audevuilli@users.noreply.github.com>
2023-05-13 11:39:08 +0100

    update config.py

--- src/acoupi/config.py
+++ src/acoupi/config.py
@@ -4,22 +4,29 @@
 LATITUDE = 51.5381
 LONGITUDE = -0.0099
 
+"""Default timezone of the deployed device"""
+DEFAULT_TIMEZONE = 'Europe/London'
+
 """Default audio recording parameters."""
 DEFAULT_SAMPLE_RATE = 192000
 DEFAULT_AUDIO_CHANNELS = 1
-DEFAULT_CHUNK_SIZE = 2048
-# DEFAULT_CHUNK_SIZE = 1024
+DEFAULT_CHUNK_SIZE = 4096
+# DEFAULT_CHUNK_SIZE = 2^x
 
 """Default start and end audio recording"""
-START_RECORDING = "15:00"
-END_RECORDING = "12:00"
+START_RECORDING = '10:00:00'
+END_RECORDING = '08:00:00'
 
 """Default audio recording lenght in seconds."""
 DEFAULT_RECORDING_DURATION = 3
 
 """Default recording interval in seconds."""
-DEFAULT_RECORDING_INTERVAL = 10
+DEFAULT_RECORDING_INTERVAL = 6
 
 """Default detection probabilities threshold"""
-DETECTION_THRESHOLD = 0.1
+DETECTION_THRESHOLD = 0.2
+
+"""Default timeformat to name the audio recording files"""
+DEFAULT_TIMEFORMAT = '%Y%m%d_%H%M%S' #Year-Month-Day Hour-Minute-Second
+
 DEVICE_INDEX = 1
