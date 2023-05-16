5b3889c1f10b760405685373abe8c4696df01150
Aude <47717187+audevuilli@users.noreply.github.com>
2023-05-16 17:35:31 +0100

    update acoupi_install.sh

--- src/acoupi/acoupi_install.sh
+++ src/acoupi/acoupi_install.sh
@@ -11,10 +11,10 @@ sudo apt install git alsa-utils libasound2-dev wget cmake sqlite3 #pulseaudio
 sudo apt install python3-dev python3-pip python3-venv python3-pyaudio
 
 # Move to home directory
-# cd ~
-# echo "Cloning Acoupi repository - Main branch" 
-# branch=main
-# git clone -b $branch --depth=1 https://github.com/audevuilli/acoupi.git ${HOME}/acoupi
+cd ~
+echo "Cloning Acoupi repository - Main branch" 
+branch=main
+git clone -b $branch --depth=1 https://github.com/audevuilli/acoupi.git ${HOME}/acoupi
 
 # Move to git directory
 # cd ~/acoupi
@@ -25,35 +25,58 @@ sudo apt install python3-dev python3-pip python3-venv python3-pyaudio
 
 # Install pyaudio using install_pyaudio.sh script
 echo "Installing pyaudio"
-bash $HOME/src/acoupi/script/install_pyaudio.sh
+bash $HOME/acoupi/src/acoupi/script/install_pyaudio.sh
 
 # Install packages with pip
 echo "Installing libraries package"
 #pip3 install -U -r $HOME/src/acoupi/requirements.txt
 pip3 install . 
 
-## Install sqlite database
-#sudo apt install sqlite3
-## Create a new database file 
-#sqlite3 acoupi.db
-## Create all the necessary tables in acoupi.db
+# Setup Configuration
+echo "Set config parameters to run acoupi"
+echo "STEP 1: Please enter the device timezone as Continent/City (e.g. Europe/London)"
+read DEFAULT_TIMEZONE
 
-# Create directory to store audio files
-#echo "Creating necessary directories"
-#sudo -u ${USER} mkdir -p audio/audio_files
-#sudo -u ${USER} mkdir -p audio/analysed
+echo "STEP 2: Please enter your microphone sampling rate (e.g. 192000)"
+read DEFAULT_SAMPLE_RATE
+
+echo "STEP 3: Please enter the daily START time for recordings."
+echo "Note that if start time is after end time - recordings will run overnight."
+echo "Enter in format hh:mm:ss (e.g 19:00:00)"
+read START_RECORDING
+
+echo "STEP 4: Please enter the daily END time for recordings."
+echo "Enter in format hh:mm:ss (e.g 08:00:00)"
+read END_RECORDING
+
+echo "STEP 5: Please enter the detection probability threshold for species detection."
+echo "Note that robustness of the probability of detection varied from species to species."
+echo "Threshold between 0 and 1. Threshold of 0.7 is recommended. Set lower/higher"
+read DEFAULT_THRESHOLD
+
+echo "STEP 6: MQTT Server Parameters. Please ignore if MQTT Server not use"
+
+# Store the configuration variables in a config file
+file="$HOME/acoupi/src/acoupi/acoupi.config"
+echo 'DEFAULT_TIMEZONE' >> $file
+echo DEFAULT_SAMPLE_RATE >> $file
+echo 'START_RECORDING' >> $file
+echo 'END_RECORDING' >> $file
+echo DEFAULT_THRESHOLD >> $file
+
+echo "Thanks! config file created at $HOME/acoupi/src/acoupi/acoupi.config"
+echo ""
 
 # Move the .service files to lib/systemd/system - Enable and Start it
-services = ("acoupi_audiorec.service" 
-            "acoupi_runmodel.service"
-            "acoupi_saveresults.service"
-            "acoupi_senddata.service")
+echo "Installing Acoupi Services"
+# Move acoupi_run.service into systemd
+#sudo ln -sf $HOME/acoupi/src/acoupi/services/acoupi_run.service /usr/lib/systemd/system
+sudo ln -sf $HOME/acoupi/src/acoupi/services/acoupi_testing.service /usr/lib/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable acoupi_testing.service
+sudo systemctl start acoupi_testing.service
+#systemctl status acoupi_testing.service
 
-for service in "${services[@]}"
-do 
-    sudo ln -sf $HOME/acoupi/services/$service /usr/lib/systemd/system
-    sudo systemctl enable $service
-    sudo systemctl start $service
-
-    systemctl is-active --quiet $service && echo acoupi services are running || echo acoupi services are not running
-done
+echo "Acoupi Setup Complete!"
+echo "***************************************"
+echo "Please reboot your device to start acoupi service - use sudo reboot"
