f8b85b8d31cec313502bb394e05fba1cdc9b9b33
Aude <47717187+audevuilli@users.noreply.github.com>
2023-05-18 15:21:52 +0100

    update audio_recorder

--- src/acoupi/audio_recorder.py
+++ src/acoupi/audio_recorder.py
@@ -59,55 +59,6 @@ class PyAudioRecorder(AudioRecorder):
         return device_index
 
 
-    ##def record(self) -> Recording:
-    ##    """Record a 3 second temporary audio file. Return the temporary path of the file."""       
-    ##    
-    ##    #device_index = self.findAudioDevice()
-    ##    self.datetime = datetime.datetime.now()
-    ##    
-    ##    #Create a temporary file to record audio
-    ##    with tempfile.NamedTemporaryFile(suffix='.wav', delete=False) as temp_audiof:
-    ##        
-    ##        # Get the temporary file path from the created temporary audio file
-    ##        temp_audio_path = temp_audiof.name
-##
-    ##        #Create an new instace of PyAudio
-    ##        p = pyaudio.PyAudio()
-    ##    
-    ##        #Open new audio stream to start recording
-    ##        stream = p.open(format=pyaudio.paInt16,
-    ##                        channels=self.channels,
-    ##                        rate=self.sample_rate,
-    ##                        input=True,
-    ##                        frames_per_buffer=self.chunk,
-    ##                        input_device_index=self.device_index)
-##
-    ##        #Initialise array to store audio frames
-    ##        frames = []
-    ##        for i in range(0, int(self.sample_rate/self.chunk*self.duration)):
-    ##            data = stream.read(self.chunk)
-    ##            frames.append(data)
-##
-    ##        #Stop Recording and close the port interface
-    ##        stream.stop_stream()
-    ##        stream.close()
-    ##        p.terminate()
-##
-    ##        #Create a WAV file to write the audio data
-    ##        temp_audio_file = wave.open(temp_audio_path, 'wb')
-    ##        temp_audio_file.setnchannels(self.channels)
-    ##        temp_audio_file.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-    ##        temp_audio_file.setframerate(self.sample_rate)
-##
-    ##        # Write the audio data to the temporary file
-    ##        temp_audio_file.writeframes(b''.join(frames))    
-    ##        temp_audio_file.close()
-##
-    ##        # Create a Recording object and return it
-    ##        recording = Recording(path=temp_audio_path, datetime=self.datetime, duration=self.duration, samplerate=self.sample_rate)
-    ##        return recording
-
-
     def record(self) -> Recording:
         """Record a 3 second temporary audio file. Return the temporary path of the file."""       
         
