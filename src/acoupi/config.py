c0801e7aeb3bc7e9ad9dfe14b28e04dee09708ce
audevuilli <aude.vuilli@gmail.com>
2023-05-30 15:14:10 +0100

    update config.py

--- src/acoupi/config.py
+++ src/acoupi/config.py
@@ -19,23 +19,23 @@ DEFAULT_CHUNK_SIZE = 8192
 
 """Default start and end audio recording"""
 START_RECORDING = '10:00:00'
-END_RECORDING = '19:00:00'
+END_RECORDING = '18:00:00'
 
 """Default audio recording length in seconds."""
 DEFAULT_RECORDING_DURATION = 3
 """Default recording interval in seconds."""
-DEFAULT_RECORDING_INTERVAL = 8
+DEFAULT_RECORDING_INTERVAL = 10
 
 """Default detection probabilities threshold"""
-DEFAULT_THRESHOLD = 0.1
+DEFAULT_THRESHOLD = 0.2
 
 """Default timeformat to name the audio recording files"""
 DEFAULT_TIMEFORMAT = '%Y%m%d_%H%M%S' #Year-Month-Day Hour-Minute-Second
 
 """Default saving recordings"""
 # Based on time intervals such as between 9pm and 10pm
-START_SAVING_RECORDING = '15:10:00'
-END_SAVING_RECORDING = '15:20:00'
+START_SAVING_RECORDING = '14:00:00'
+END_SAVING_RECORDING = '12:30:00'
 
 # Based on duration after dawn and dusk time (duratino in minutes)
 SAVE_DAWNDUSK_DURATION = 10
