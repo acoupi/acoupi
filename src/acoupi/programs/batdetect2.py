4e3ad5520e2b0b74310c0ef05cce85a2df4a213b
Aude <47717187+audevuilli@users.noreply.github.com>
2023-06-23 15:22:18 +0100

    Create configschema for Batdetect2 & Add templates.py for tasks

--- src/acoupi/programs/batdetect2.py
+++ src/acoupi/programs/batdetect2.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional
+from pathlib import Path
 
 from acoupi.config_schemas import BaseConfigSchema
 from acoupi.programs.base import AcoupiProgram
@@ -27,32 +28,40 @@ class RecordingSaving(BaseModel):
 
     starttime_saving_recording: Optional[datetime.time] = START_SAVING_RECORDING
     endtime_saving_recording: Optional[datetime.time] = END_SAVING_RECORDING
-    timeafter_dawndusk: Optional[int] = AFTER_DAWNDUSK_DURATION 
-    timeafter_sunset: Optional[int] = BEFORE_DAWNDUSK_DURATION
+    before_dawndusk_duration: Optional[int] = BEFORE_DAWNDUSK_DURATION 
+    after_dawndusk_duration: Optional[int] = AFTER_DAWNDUSK_DURATION
     saving_frequency_duration: Optional[int] = SAVE_FREQUENCY_DURATION
     saving_frequency_interval: Optional[int] = SAVE_FREQUENCY_INTERVAL
 
+class AudioDirectories(BaseModel):
+    "Audio Recording Directories configuration"
+
+    audio_dir_true: Optional[Path] = DIR_RECORDING_TRUE
+    audio_dir_false: Optional[Path] = DIR_RECORDING_FALSE
 
 class MessageConfig(BaseModel):
     "MQTT configuration to send messages"
 
     host: str = DEFAULT_MQTT_HOST
     port: int = DEFAULT_MQTT_PORT
-    client_username: Optional[str] = DEFAULT_MQTT_CLIENT_USER
     client_password: Optional[str] = DEFAULT_MQTT_CLIENT_PASS
+    client_username: Optional[str] = DEFAULT_MQTT_CLIENT_USER
     topic: Optional[str] = DEFAULT_MQTT_TOPIC
     clientid: Optional[str] = DEFAULT_MQTT_CLIENTID
 
 
-class BatDetect2ConfigSchema(BaseConfigSchema):
+class BatDetect2_ConfigSchema(BaseConfigSchema):
     """BatDetect2 Configuration Schematic"""
 
     name: str = "batdetect2"
+
+    threshold: float = DEFAULT_THRESHOLD
     
     audio_config: AudioConfig = Field(default_factory=AudioConfig)
     recording_schedule: RecordingSchedule = Field(default_factory=RecordingSchedule)
     recording_saving: RecordingSaving = Field(default_factory=RecordingSaving)
-    message_config = Field(default_factory=MessageConfig)
+    audio_directories: AudioDirectories = Field(default_factory=AudioDirectories)
+    message_config: MessageConfig = Field(default_factory=MessageConfig)
 
 
     @classmethod
@@ -73,6 +82,7 @@ class BatDetect2ConfigSchema(BaseConfigSchema):
             type=str, 
             default=DEFAULT_TIMEZONE,
         )
+        """ Microphone Configuration Arguments"""
         parser.add_arguments(
             "--samplerate", 
             type=int, 
@@ -88,8 +98,126 @@ class BatDetect2ConfigSchema(BaseConfigSchema):
             type=int, 
             default=DEFAULT_CHUNKSIZE,
         )
+        """ Audio Recordings Configuration Arguments"""
         parser.add_arguments(
-            "--duration",
+            "--audio_duration",
             type=int, 
             default=DEFAULT_DURATION
         )
+        parser.add_arguments(
+            "--audio_interval",
+            type=int, 
+            default=DEFAULT_INTERVAL
+        )
+        parser.add_arguments(
+            "--starttime",
+            type=datetime.time.fromisoformat, 
+            default=START_RECORDING_TIME
+        )
+        parser.add_arguments(
+            "--endtime",
+            type=datetime.time.fromisoformat, 
+            default=END_RECORDING_TIME
+        )
+        parser.add_arguments(
+            "--threshold", 
+            type=float, 
+            default=DEFAULT_THRESHOLD,
+        )
+        ## TODO: Add Saving Recording Agruments
+        """Saving Recording Configuration Arguments"""
+        parser.add_arguments(
+            "--before_dawndusk",
+            type=str, 
+            default=BEFORE_DAWNDUSK_DURATION
+        )
+        parser.add_arguments(
+            "--after_dawndusk",
+            type=str, 
+            default=AFTER_DAWNDUSK_DURATION
+        )
+        parser.add_arguments(
+            "--audiodir_true",
+            type=Path, 
+            default=DIR_RECORDING_TRUE
+        )
+        parser.add_arguments(
+            "--audiodir_false",
+            type=Path, 
+            default=DIR_RECORDING_FALSE
+        )
+        """ MQTT Configuration Arguments"""
+        parser.add_arguments(
+            "--host",
+            type=str, 
+            default=DEFAULT_MQTT_HOST
+        )
+        parser.add_arguments(
+            "--port",
+            type=int, 
+            default=DEFAULT_MQTT_PORT
+        )
+        parser.add_arguments(
+            "--client_password",
+            type=str, 
+            default=DEFAULT_MQTT_CLIENT_PASS
+        )
+        parser.add_arguments(
+            "--client_username",
+            type=int, 
+            default=DEFAULT_MQTT_CLIENT_USER
+        )
+        parser.add_arguments(
+            "--topic",
+            type=str, 
+            default=DEFAULT_MQTT_TOPIC
+        )
+        parser.add_arguments(
+            "--clientid",
+            type=int, 
+            default=DEFAULT_MQTT_CLIENTID
+        )
+
+    
+    @classmethod
+    def from_parsed_args(cls, args): 
+        """Create config from arguments"""
+        return cls(
+            audio_config=AudioConfig(
+                samplerate=args.samplerate,
+                audio_channels=args.audio_channels,
+                chunksize=args.chunksize,
+                audio_duration=args.audio_duration,
+                audio_interval=args.audio_interval,
+            ),
+            recording_schedule=RecordingSchedule(
+                start_time=args.starttime,
+                end_time=args.endtime,
+            ),
+            recording_saving=RecordingSaving(
+                before_dawndusk_duration=args.before_dawndusk,
+                eafter_dawndusk_duration=args.after_dawndusk,
+            ),
+            audio_directories=AudioDirectories(
+                audiodir_true=args.audiodir_true,
+                audiodir_false=args.audiodir_false,
+            ),
+            message_config=MessageConfig(
+                host=args.host,
+                port=args.port, 
+                client_password=args.client_password, 
+                client_username=args.client_username, 
+                topic=args.topic,
+                clientid=args.clientid,
+            ),
+            detection_threshold=args.threshold,
+        )
+
+class BatDetect2_Program(AcoupiProgram):
+    """BatDetect2 Program"""
+
+    config: BatDetect2_ConfigSchema
+
+    def setup(sefl, config: BatDetect2_ConfigSchema):
+        """Setup"""
+        
---
+++ src/acoupi/tasks/templates.py
@@ -0,0 +1,188 @@
+"""Process templates for Acoupi.
+
+Acoupi offers a collection of process templates to assist in the creation of
+recording, detecting, and data sending processes. While Acoupi includes a
+variety of components to construct these processes, users may prefer to use
+their own components. By utilizing the provided templates, users can ensure
+that their custom processes integrate with Acoupi and adhere to standardized
+building practices. The use of templates also allows for effortless
+customization of processes.
+
+The templates provided take the form of functions that return a function that
+can be used to start a process. Each template takes a set of arguments that
+are used to construct the process. The arguments are Acoupi components
+of the appropriate type, such as a message store, messenger, model, etc.
+Any object that implements the appropriate interface can be used as an
+argument. This allows users to use out-of-the-box components or components
+that they have created themselves.
+"""
+import datetime
+import logging
+from typing import Callable, List, Optional
+
+from acoupi import data
+from acoupi.components import types
+from acoupi.files import delete_recording, get_temp_file_id, get_temp_files
+
+logger = logging.getLogger(__name__)
+
+
+def generate_recording_task(
+    recorder: types.AudioRecorder,
+    store: types.Store,
+    logger: logging.Logger = logger,
+    recording_conditions: Optional[List[types.RecordingCondition]] = None,
+) -> Callable[[], Optional[data.Recording]]:
+    """Generate a recording task."""
+
+    def recording_task() -> Optional[data.Recording]:
+        """Record audio."""
+        now = datetime.datetime.now()
+        logger.info("Starting recording process")
+
+        # Check if recording conditions are met
+        if not all(
+            condition.should_record(now)
+            for condition in recording_conditions or []
+        ):
+            # If conditions not met exit process
+            logger.info("Recording conditions not met.")
+            return
+
+        # Get current deployment
+        deployment = store.get_current_deployment()
+
+        # Record audio
+        logger.info("Recording audio")
+        recording = recorder.record()
+
+        # Store recording metadata
+        store.store_recording(recording, deployment)
+        logger.info("Recording metadata stored")
+
+        return recording
+
+    return recording_task
+
+
+def generate_detection_task(
+    store: types.Store,
+    model: types.Model,
+    message_store: types.MessageStore,
+    logger: logging.Logger = logger,
+    output_cleaners: Optional[List[types.ModelOutputCleaner]] = None,
+    processing_filters: Optional[List[types.ProcessingFilter]] = None,
+    message_factories: Optional[List[types.ModelOutputMessageBuilder]] = None,
+) -> Callable[[data.Recording], None]:
+    """Generate a detection task."""
+
+    def detection_task(recording: data.Recording) -> None:
+        """Detect events in audio."""
+        logger.info("Starting detection process")
+
+        # Check if recording should be processed
+        if not all(
+            filter.should_process_recording(recording)
+            for filter in processing_filters or []
+        ):
+            # If recording should not be processed exit process
+            logger.info("Recording should not be processed, skipping")
+            return
+
+        # Detect events in recordings
+        logger.info("Detecting events in recordings")
+        model_output = model.run(recording)
+
+        # Clean model output
+        logger.info("Cleaning model output")
+        for cleaner in output_cleaners or []:
+            model_output = cleaner.clean(model_output)
+
+        # Store detections
+        store.store_model_output(model_output)
+
+        # Create messages
+        for message_factory in message_factories or []:
+            message = message_factory.build_message(model_output)
+            message_store.store_message(message)
+
+    return detection_task
+
+
+def build_file_management_process(
+    store: types.Store,
+    file_manager: types.RecordingSavingManager,
+    file_filters: Optional[List[types.RecordingSavingFilter]] = None,
+) -> Callable[[], None]:
+    """Build a process to manage files.
+
+    Use this function to build a process that will manage files using your
+    preferred file manager and store. This function will return a function
+    that can be used to start the process.
+
+    Recordings are temporarily stored on the memory to reduce the number of
+    writes to the disk. This process will move recordings from the memory to
+    the disk, and remove recordings that are no longer needed.
+    """
+
+    def file_management_process() -> None:
+        """Manage files."""
+        logger.info("Starting file management process")
+
+        temp_wav_files = get_temp_files()
+        ids = [get_temp_file_id(path) for path in temp_wav_files]
+        recordings_and_outputs = store.get_recordings(ids=ids)
+
+        for recording, model_outputs in recordings_and_outputs:
+            if recording.path is None:
+                logger.error("Temporary recording has no path")
+                continue
+
+            if not all(
+                filter.should_save_recording(recording, model_outputs)
+                for filter in file_filters or []
+            ):
+                logger.info("Recording should not be saved, deleting")
+                delete_recording(recording.path)
+                continue
+
+            path = file_manager.save_recording(
+                recording,
+                model_outputs=model_outputs,
+            )
+
+            store.update_recording_path(recording, path)
+
+    return file_management_process
+
+
+def build_send_data_process(
+    message_store: types.MessageStore,
+    messenger: types.Messenger,
+) -> Callable[[], None]:
+    """Build a process to send data to a remote server.
+
+    Use this function to build a process that will send data to a remote server
+    using your preferred messenger and message store. This function will return
+    a function that can be used to start the process.
+
+    The built process will send unsynced deployments, recordings, and detections
+    to the remote server. The process will then store the response from the
+    remote server, so that the data can be marked as synced.
+
+    Args:
+        message_store: The message store to use. The message store
+            is used to get unsynced data and store any messages that are sent.
+        messenger: The messenger to use.
+
+    Returns:
+        A function that can be used to start the process.
+    """
+
+    def send_data_process() -> None:
+        """Process to sync data."""
+        for message in message_store.get_unsent_messages():
+            response = messenger.send_message(message)
+            message_store.store_response(response)
+
+    return send_data_process
