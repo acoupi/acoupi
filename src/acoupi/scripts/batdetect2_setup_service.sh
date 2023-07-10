4e5ef5fd133a66c4d27613975d45bad1c432e45b
Aude <47717187+audevuilli@users.noreply.github.com>
2023-07-10 09:50:36 +0100

    update run_batdetect2.service

--- src/acoupi/scripts/batdetect2_setup_service.sh
+++ src/acoupi/scripts/batdetect2_setup_service.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-sudo ln -sf $HOME/acoupi/src/acoupi/services/run_acoupi.service /usr/lib/systemd/system
+sudo ln -sf $HOME/acoupi/src/acoupi/services/run_batdetect2.service /usr/lib/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable run_acoupi.service
-sudo systemctl start run_acoupi.service
-systemctl status run_acoupi.service
+sudo systemctl enable run_batdetect2.service
+sudo systemctl start run_batdetect2.service
+systemctl status run_batdetect2.service
