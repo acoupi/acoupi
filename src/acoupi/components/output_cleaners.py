b6cc467cecdd05d50a2953760b7d9f45948c17d2
Santiago Martinez <santiago.mbal@gmail.com>
2023-07-10 13:32:12 +0100

    Fixed ThresholdCleaner

--- src/acoupi/components/output_cleaners.py
+++ src/acoupi/components/output_cleaners.py
@@ -1,50 +1,64 @@
 """Model output cleaners."""
-from typing import List
+from typing import List, Optional
 
 from acoupi.components.types import ModelOutputCleaner
-from acoupi.data import Detection, ModelOutput, PredictedTag
+from acoupi.data import Detection, ModelOutput
+
 
 class ThresholdDetectionFilter(ModelOutputCleaner):
     """Keeps predictions with a probability above a threshold.
 
     This class implements a model output cleaner that removes any
-    predictions with a probability below a threshold. This includes
-    removing low confidence tags and detections.
-    """
-    
-    def __init__(self, threshold: float):
-        """Initiatlise the filter.
-        
-        Args: 
-            threshold: The probability threshold to be used.
-        """ 
-        self.threshold = threshold
+    predictions with a probability below a threshold. This only includes
+    removing detections.
 
-    def get_clean_tags(self, tags: List[PredictedTag]) -> List[PredictedTag]:
-        """Removes tags with low probability."""
-        return [
-            tag for tag in tags 
-            if tag.probability >= self.threshold
-        ]
+    Additionally it can be configured to only keep detections that have a
+    tag with a specific key. This is useful when the model detects
+    non-species specific tags, such as "noise" or "unknown" tags, and you
+    only want to keep detections that have a species specific tag.
+    """
+
+    def __init__(self, threshold: float, tag_keys: Optional[List[str]] = None):
+        """Initialise the filter.
+
+        Parameters
+        ----------
+        threshold: float
+            The probability threshold to be used.
+
+        tag_keys: Optional[List[str]]
+            Tag keys to be monitored. Will only keep detections that have a tag
+            from with one of the specified keys. The tag probability is not
+            considered when filtering. If None, detections will be kept
+            regardless of the tag key. Defaults to None.
+
+        """
+        self.threshold = threshold
+        self.tag_keys = tag_keys
 
     def get_clean_detections(
         self, detections: List[Detection]
     ) -> List[Detection]:
         """Removes detections with low probability."""
         return [
-            self.clean_detection(detection)
+            detection
             for detection in detections
-            if detection.probability >= self.threshold
+            if not self.should_remove_detection(detection)
         ]
 
-    def clean_detection(self, detection: Detection) -> Detection:
-        """Removes tags with low probability from detection."""
-        return Detection(
-            id=detection.id,
-            location=detection.location,
-            probability=detection.probability,
-            tags=self.get_clean_tags(detection.tags),
-        )
+    def should_remove_detection(self, detection: Detection) -> bool:
+        """Determine if detection should be removed."""
+        if detection.probability < self.threshold:
+            return True
+
+        if self.tag_keys is None:
+            return False
+
+        for tag in detection.tags:
+            if tag.tag.key in self.tag_keys:
+                return False
+
+        return True
 
     def clean(self, model_output: ModelOutput) -> ModelOutput:
         """Clean the model output.
@@ -61,10 +75,9 @@ class ThresholdDetectionFilter(ModelOutputCleaner):
         return ModelOutput(
             model_name=model_output.model_name,
             recording=model_output.recording,
-            tags=self.get_clean_tags(model_output.tags),
+            tags=model_output.tags,
             detections=self.get_clean_detections(model_output.detections),
         )
-
 
 
 # TODO: Update this class to a ModelOutputCleaner and new ModelOutput and
---
+++ tests/test_components/test_output_cleaners.py
@@ -0,0 +1,225 @@
+"""Test output cleaners."""
+
+import datetime
+from pathlib import Path
+
+import pytest
+
+from acoupi import data
+from acoupi.components import output_cleaners
+
+
+@pytest.fixture
+def create_test_model_output():
+    """Return a model output."""
+    deployment = data.Deployment(
+        name="test_deployment",
+    )
+
+    recording = data.Recording(
+        path=Path("test.wav"),
+        duration=1,
+        samplerate=256000,
+        audio_channels=1,
+        deployment=deployment,
+        datetime=datetime.datetime.now(),
+    )
+
+    def factory(
+        detections: list[data.Detection],
+    ) -> data.ModelOutput:
+        """Return a model output."""
+        return data.ModelOutput(
+            recording=recording,
+            model_name="test_model",
+            detections=detections,
+        )
+
+    return factory
+
+
+@pytest.fixture
+def create_test_detection():
+    """Fixture for creating random detections.
+
+    Will create detections with no location and a single tag.
+    """
+
+    def factory(
+        tag_value: str,
+        detection_probability: float = 0.5,
+        tag_probability: float = 1,
+        tag_key: str = "species",
+    ) -> data.Detection:
+        """Return a random detection."""
+        return data.Detection(
+            probability=detection_probability,
+            tags=[
+                data.PredictedTag(
+                    tag=data.Tag(
+                        value=tag_value,
+                        key=tag_key,
+                    ),
+                    probability=tag_probability,
+                ),
+            ],
+        )
+
+    return factory
+
+
+def test_threshold_detection_filter_removes_detections_with_low_confidence(
+    create_test_model_output,
+    create_test_detection,
+):
+    """Test threshold filter removes detections with low confidence."""
+    # Arrange
+    cleaner = output_cleaners.ThresholdDetectionFilter(
+        threshold=0.5,
+    )
+    model_output = create_test_model_output(
+        detections=[
+            create_test_detection(
+                "species1",
+                detection_probability=0.1,
+                tag_probability=0.5,
+            ),
+            create_test_detection(
+                "species2",
+                detection_probability=0.6,
+                tag_probability=0.5,
+            ),
+        ]
+    )
+
+    # Act
+    cleaned_model_output = cleaner.clean(model_output)
+
+    # Assert
+    assert len(cleaned_model_output.detections) == 1
+    assert cleaned_model_output.detections[0].tags[0].tag.value == "species2"
+
+
+def test_threshold_detection_filter_removes_non_species_detections(
+    create_test_model_output,
+    create_test_detection,
+):
+    """Test threshold filter removes non-species detections."""
+    # Arrange
+    cleaner = output_cleaners.ThresholdDetectionFilter(
+        threshold=0.5,
+        tag_keys=["species"],
+    )
+    model_output = create_test_model_output(
+        detections=[
+            create_test_detection(
+                "echolocation",
+                tag_key="event",
+                detection_probability=0.7,
+                tag_probability=0.5,
+            ),
+            create_test_detection(
+                "species2",
+                detection_probability=0.6,
+                tag_probability=0.5,
+            ),
+        ]
+    )
+
+    # Act
+    cleaned_model_output = cleaner.clean(model_output)
+
+    # Assert
+
+    assert len(cleaned_model_output.detections) == 1
+    assert cleaned_model_output.detections[0].tags[0].tag.value == "species2"
+
+
+def test_threshold_detection_keeps_detections_even_with_low_confidence_tags(
+    create_test_model_output,
+    create_test_detection,
+):
+    """Test threshold filter keeps detections even with low confidence tags."""
+    cleaner = output_cleaners.ThresholdDetectionFilter(
+        threshold=0.5,
+    )
+    model_output = create_test_model_output(
+        detections=[
+            create_test_detection(
+                "species1",
+                detection_probability=0.7,
+                tag_probability=0.1,
+            ),
+            create_test_detection(
+                "species2",
+                detection_probability=0.6,
+                tag_probability=0.5,
+            ),
+        ]
+    )
+
+    # Act
+    cleaned_model_output = cleaner.clean(model_output)
+
+    # Assert
+    assert len(cleaned_model_output.detections) == 2
+
+
+def test_threshold_removes_detections_with_default_tag_probability(
+    create_test_model_output,
+    create_test_detection,
+):
+    """Test threshold filter removes detections with default tag probability."""
+    # Arrange
+    cleaner = output_cleaners.ThresholdDetectionFilter(
+        threshold=0.5,
+    )
+    model_output = create_test_model_output(
+        detections=[
+            create_test_detection(
+                "species1",
+                detection_probability=0.7,
+                tag_probability=0.5,
+            ),
+            create_test_detection(
+                "species2",
+                detection_probability=0.3,
+            ),
+        ]
+    )
+
+    # Act
+    cleaned_model_output = cleaner.clean(model_output)
+
+    # Assert
+
+    assert len(cleaned_model_output.detections) == 1
+    assert cleaned_model_output.detections[0].tags[0].tag.value == "species1"
+
+
+def test_threshold_does_not_remove_tags_even_if_with_low_probability_score(
+    create_test_model_output,
+    create_test_detection,
+):
+    """Test threshold filter removes detections with default tag probability."""
+    # Arrange
+    cleaner = output_cleaners.ThresholdDetectionFilter(
+        threshold=0.5,
+    )
+    model_output = create_test_model_output(
+        detections=[
+            create_test_detection(
+                "species1",
+                detection_probability=0.7,
+                tag_probability=0.1,
+            ),
+        ]
+    )
+
+    # Act
+    cleaned_model_output = cleaner.clean(model_output)
+
+    # Assert
+    assert len(cleaned_model_output.detections) == 1
+    assert len(cleaned_model_output.detections[0].tags) == 1
+    assert cleaned_model_output.detections[0].tags[0].tag.value == "species1"
