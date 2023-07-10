f945e85333c8d90978d76cfc78c1e683d2e08aff
Aude <47717187+audevuilli@users.noreply.github.com>
2023-07-10 21:59:53 +0100

    rename template

---
+++ src/acoupi/template_config_iotmessages.py
@@ -0,0 +1,22 @@
+"""Template configuration for device communication."""
+
+"""HTTP POST Request"""
+DEFAULT_BASEURL = 'your-base-url'
+DEFAULT_TOPIC = 'your-topic'
+DFAULT_DEVICE_ID = 'your-deviceid'
+DEFAULT_CLIENTID = 'your-clientid'
+DEFAULT_PASS = 'your-password'
+DEFAULT_HEADERS = "{Authorization':'api-key'}"
+
+"""Configuration file with default configuration for MQTT."""
+
+DEFAULT_MQTT_HOST = 'your-default-host'
+DEFAULT_MQTT_PORT = XXXX 
+
+"""Default username and password"""
+DEFAULT_MQTT_CLIENT_USER = 'your-mqtt-username'
+DEFAULT_MQTT_CLIENT_PASS = 'your-mqtt-password'
+
+DEFAULT_MQTT_CLIENTID = 'your-clientid'
+DEFAULT_MQTT_TOPIC = 'your-topic'
+
