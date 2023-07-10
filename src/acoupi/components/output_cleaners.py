0906e7102d623d9c4616f1c078c2b0a3f4db9889
Santiago Martinez <santiago.mbal@gmail.com>
2023-07-10 17:49:14 +0100

    Separated output cleaners into multiple composable components

--- src/acoupi/components/output_cleaners.py
+++ src/acoupi/components/output_cleaners.py
@@ -1,24 +1,19 @@
 """Model output cleaners."""
-from typing import List, Optional
+from typing import List
 
+from acoupi import data
 from acoupi.components.types import ModelOutputCleaner
-from acoupi.data import Detection, ModelOutput
 
 
-class ThresholdDetectionFilter(ModelOutputCleaner):
+class DetectionProbabilityCleaner(ModelOutputCleaner):
     """Keeps predictions with a probability above a threshold.
 
     This class implements a model output cleaner that removes any
     predictions with a probability below a threshold. This only includes
-    removing detections.
-
-    Additionally it can be configured to only keep detections that have a
-    tag with a specific key. This is useful when the model detects
-    non-species specific tags, such as "noise" or "unknown" tags, and you
-    only want to keep detections that have a species specific tag.
+    removing detections and not predicted tags at the recording level.
     """
 
-    def __init__(self, threshold: float, tag_keys: Optional[List[str]] = None):
+    def __init__(self, threshold: float):
         """Initialise the filter.
 
         Parameters
@@ -26,41 +21,78 @@ class ThresholdDetectionFilter(ModelOutputCleaner):
         threshold: float
             The probability threshold to be used.
 
-        tag_keys: Optional[List[str]]
-            Tag keys to be monitored. Will only keep detections that have a tag
-            from with one of the specified keys. The tag probability is not
-            considered when filtering. If None, detections will be kept
-            regardless of the tag key. Defaults to None.
-
         """
         self.threshold = threshold
-        self.tag_keys = tag_keys
 
     def get_clean_detections(
-        self, detections: List[Detection]
-    ) -> List[Detection]:
+        self, detections: List[data.Detection]
+    ) -> List[data.Detection]:
         """Removes detections with low probability."""
         return [
             detection
             for detection in detections
-            if not self.should_remove_detection(detection)
+            if detection.probability >= self.threshold
         ]
 
-    def should_remove_detection(self, detection: Detection) -> bool:
-        """Determine if detection should be removed."""
-        if detection.probability < self.threshold:
-            return True
+    def clean(self, model_output: data.ModelOutput) -> data.ModelOutput:
+        """Clean the model output.
 
-        if self.tag_keys is None:
-            return False
+        This methods will remove any detection with a probability below the
+        threshold.
 
-        for tag in detection.tags:
-            if tag.tag.key in self.tag_keys:
-                return False
+        Parameters
+        ----------
+        model_output: data.ModelOutput
+            The model output to clean.
 
-        return True
+        Returns
+        -------
+        data.ModelOutput
+            The cleaned model output.
+        """
+        return data.ModelOutput(
+            model_name=model_output.model_name,
+            recording=model_output.recording,
+            tags=model_output.tags,
+            detections=self.get_clean_detections(model_output.detections),
+        )
 
-    def clean(self, model_output: ModelOutput) -> ModelOutput:
+
+class TagProbabilityThresholdCleaner(ModelOutputCleaner):
+    """Keeps tags with a probability above a threshold.
+
+    This class implements a model output cleaner that removes any predicted
+    tags with a probability below a threshold. This only includes removing
+    predicted tags, and will not remove any detection.
+    """
+
+    def __init__(self, threshold: float):
+        """Initialise the filter.
+
+        Parameters
+        ----------
+        threshold: float
+            The probability threshold to be used.
+
+        """
+        self.threshold = threshold
+
+    def get_clean_tags(
+        self, tags: List[data.PredictedTag]
+    ) -> List[data.PredictedTag]:
+        """Removes tags with low probability."""
+        return [tag for tag in tags if tag.probability >= self.threshold]
+
+    def get_clean_detection(self, detection: data.Detection) -> data.Detection:
+        """Removes tags with low probability."""
+        return data.Detection(
+            id=detection.id,
+            location=detection.location,
+            probability=detection.probability,
+            tags=self.get_clean_tags(detection.tags),
+        )
+
+    def clean(self, model_output: data.ModelOutput) -> data.ModelOutput:
         """Clean the model output.
 
         This methods will remove any predicted tag or detection with a
@@ -72,11 +104,96 @@ class ThresholdDetectionFilter(ModelOutputCleaner):
         Returns:
             The cleaned model output.
         """
-        return ModelOutput(
+        return data.ModelOutput(
             model_name=model_output.model_name,
             recording=model_output.recording,
-            tags=model_output.tags,
-            detections=self.get_clean_detections(model_output.detections),
+            tags=self.get_clean_tags(model_output.tags),
+            detections=[
+                self.get_clean_detection(detection)
+                for detection in model_output.detections
+            ],
+        )
+
+
+class TagKeyCleaner(ModelOutputCleaner):
+    """Cleaner that only keeps detections that have a tag with a specific keys.
+
+    Optionally can remove all other tags from the detections.
+    """
+
+    def __init__(self, tag_keys: List[str], remove_other_tags: bool = True):
+        """Initialise the filter.
+
+        Parameters
+        ----------
+        tag_keys: List[str]
+            The tag keys to keep.
+
+        remove_other_tags: bool
+            If True, all tags that are not in the tag_keys will be removed from
+            the detections. Defaults to True.
+
+        """
+        self.tag_keys = tag_keys
+        self.remove_other_tags = remove_other_tags
+
+    def clean_tags(
+        self, tags: List[data.PredictedTag]
+    ) -> List[data.PredictedTag]:
+        """Remove tags that are not in the tag_keys."""
+        if not self.remove_other_tags:
+            return tags
+
+        return [tag for tag in tags if tag.tag.key in self.tag_keys]
+
+    def should_keep_detection(self, detection: data.Detection) -> bool:
+        """Determine if detection should be kept."""
+        for tag in detection.tags:
+            if tag.tag.key in self.tag_keys:
+                return True
+
+        return False
+
+    def clean_detection(self, detection: data.Detection) -> data.Detection:
+        """Remove tags that are not in the tag_keys."""
+        return data.Detection(
+            id=detection.id,
+            location=detection.location,
+            probability=detection.probability,
+            tags=self.clean_tags(detection.tags),
+        )
+
+    def clean_detections(
+        self, detections: List[data.Detection]
+    ) -> List[data.Detection]:
+        """Remove detections that do not have a tag with a key in the tag_keys."""
+        return [
+            self.clean_detection(detection)
+            for detection in detections
+            if self.should_keep_detection(detection)
+        ]
+
+    def clean(self, model_output: data.ModelOutput) -> data.ModelOutput:
+        """Clean the model output.
+
+        This methods will remove any detections that do not have a tag
+        with a key in the tag_keys list.
+
+        If remove_other_tags is True, all tags that are not in the
+        tag_keys will be removed from the detections.
+
+        Args:
+            model_output: The model output to clean.
+
+        Returns:
+            The cleaned model output.
+        """
+        return data.ModelOutput(
+            id=model_output.id,
+            model_name=model_output.model_name,
+            recording=model_output.recording,
+            tags=self.clean_tags(model_output.tags),
+            detections=self.clean_detections(model_output.detections),
         )
 
 
