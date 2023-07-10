1d99e9cecd6ef765a0cc88e96874bce009ca6b52
Santiago Martinez <santiago.mbal@gmail.com>
2023-07-10 13:40:44 +0100

    More conditions to check for multiple interval recording condition

--- tests/test_components/test_recording_conditions.py
+++ tests/test_components/test_recording_conditions.py
@@ -69,35 +69,14 @@ def test_multiple_interval_recording_manager():
         microsecond=0,
     )
 
-    assert (
-        recording_manager.should_record(now.replace(hour=9, minute=59))
-        is False
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=10, minute=0)) is True
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=10, minute=30))
-        is True
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=11, minute=0)) is True
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=11, minute=1))
-        is False
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=12, minute=0)) is True
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=12, minute=30))
-        is True
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=13, minute=0)) is True
-    )
-    assert (
-        recording_manager.should_record(now.replace(hour=13, minute=1))
-        is False
-    )
+    assert not recording_manager.should_record(now.replace(hour=4, minute=40))
+    assert not recording_manager.should_record(now.replace(hour=9, minute=59))
+    assert recording_manager.should_record(now.replace(hour=10, minute=0))
+    assert recording_manager.should_record(now.replace(hour=10, minute=30))
+    assert recording_manager.should_record(now.replace(hour=11, minute=0))
+    assert not recording_manager.should_record(now.replace(hour=11, minute=1))
+    assert recording_manager.should_record(now.replace(hour=12, minute=0))
+    assert recording_manager.should_record(now.replace(hour=12, minute=30))
+    assert recording_manager.should_record(now.replace(hour=13, minute=0))
+    assert not recording_manager.should_record(now.replace(hour=13, minute=1))
+    assert not recording_manager.should_record(now.replace(hour=20, minute=30))
