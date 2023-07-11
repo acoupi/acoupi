c77f669c66ea2d7d781c9e55792ada13728c5dfd
Aude <47717187+audevuilli@users.noreply.github.com>
2023-07-11 10:04:49 +0100

    update rename_acoupidb.sh

--- src/acoupi/clean_system/rename_acoupidb.sh
+++ src/acoupi/clean_system/rename_acoupidb.sh
@@ -4,13 +4,13 @@
 #month=$(date +%B | tr '[:upper:]' '[:lower:]')
 month=$(date +%m)
 year=$(date +%y)
-echo month
-echo year
+echo $month
+echo $year
 renamed_file="acoupi_${month}${year}.db"
-echo renamed_file
+echo $renamed_file
 
 # Move the acoupi.db
 cd ~/acoupi/src/acoupi/components/message_stores/sqlite
-mv acoupi.db "$renamed_file"
+mv acoupi.db "old_acoupidb/$renamed_file"
 cd ~/acoupi/src/acoupi/components/stores/sqlite
-mv acoupi.db "$renamed_file"
+mv acoupi.db "old_acoupidb/$renamed_file"
