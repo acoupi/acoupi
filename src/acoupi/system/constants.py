66f664ac1751b91e9ecb5967fe0c343f14325ab1
Santiago Martinez <santiago.mbal@gmail.com>
2023-08-04 22:12:40 +0100

    Moved all system constants to constants module

--- src/acoupi/system/constants.py
+++ src/acoupi/system/constants.py
@@ -3,6 +3,13 @@ import os
 from pathlib import Path
 
 ACOUPI_HOME = Path(os.environ.get("ACOUPI_HOME", str(Path.home() / ".acoupi")))
-
-PROGRAM_PATH = ACOUPI_HOME / "app.py"
-CONFIG_PATH = ACOUPI_HOME / "config.json"
+APP_NAME = "app"
+PROGRAM_PATH = ACOUPI_HOME / (APP_NAME + ".py")
+PROGRAM_CONFIG_FILE = ACOUPI_HOME / "config.json"
+CELERY_CONFIG_PATH = ACOUPI_HOME / "etc" / "conf.d" / "celery"
+RUN_DIR = ACOUPI_HOME / "run"
+LOG_DIR = ACOUPI_HOME / "log"
+LOG_LEVEL = "INFO"
+START_SCRIPT_PATH = ACOUPI_HOME / "bin" / "acoupi-workers-start.sh"
+STOP_SCRIPT_PATH = ACOUPI_HOME / "bin" / "acoupi-workers-stop.sh"
+RESTART_SCRIPT_PATH = ACOUPI_HOME / "bin" / "acoupi-workers-restart.sh"
