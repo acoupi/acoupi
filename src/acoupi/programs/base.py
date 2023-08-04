2c658c96e9909bd9679d393918e63b7e5504f940
Santiago Martinez <santiago.mbal@gmail.com>
2023-08-04 16:36:30 +0100

    Created AcoupiWorker class to keep track of worker configurations

--- src/acoupi/programs/base.py
+++ src/acoupi/programs/base.py
@@ -8,6 +8,7 @@ from celery import Celery, group
 from celery.schedules import crontab
 
 from acoupi.config_schemas import BaseConfigSchema
+from acoupi.programs.workers import AcoupiWorker, WorkerConfig
 
 B = TypeVar("B")
 A = TypeVar("A", bound=BaseConfigSchema)
@@ -21,6 +22,8 @@ class AcoupiProgram(Generic[A], ABC):
     """A program is a collection of tasks."""
 
     config: A
+
+    worker_config: Optional[WorkerConfig] = None
 
     app: Celery
 
@@ -56,11 +59,29 @@ class AcoupiProgram(Generic[A], ABC):
         """Get the config class."""
         return cls.__annotations__["config"]
 
+    @classmethod
+    def get_worker_config(cls) -> WorkerConfig:
+        """Get the worker config class."""
+        if cls.worker_config is None:
+            return WorkerConfig(workers=[AcoupiWorker(name="acoupi")])
+
+        return cls.worker_config
+
+    @classmethod
+    def get_queue_names(cls) -> list[str]:
+        """Get the queue names."""
+        return [
+            q
+            for worker in cls.get_worker_config().workers
+            for q in worker.queues
+        ]
+
     def add_task(
         self,
         function: Callable[[], Optional[B]],
         callbacks: Optional[list[Callable[[B], None]]] = None,
         schedule: Union[int, datetime.timedelta, crontab, None] = None,
+        queue: Optional[str] = None,
     ):
         """Add a task to the program."""
         if not callbacks:
@@ -68,25 +89,50 @@ class AcoupiProgram(Generic[A], ABC):
 
         callback_tasks = []
         for callback in callbacks:
-            callback_task = self.app.task(callback, name=callback.__name__)
-            callback_tasks.append(callback_task)
-            self.tasks[callback_task.name] = callback_task
+            if callback.__name__ not in self.tasks:
+                # register the callback as a task with the app
+                callback_task = self.app.task(callback, name=callback.__name__)
+                self.tasks[callback_task.name] = callback_task
 
+            # get the task from the list of tasks
+            callback_task = self.tasks[callback.__name__]
+
+            # add the task to the list of callback tasks
+            callback_tasks.append(callback_task)
+
+        # create a decorated function that will run the task and the callbacks
         @wraps(function)
         def decorated_function():
             result = function()
 
             if result is None:
+                # the task did not return anything do not run the callbacks
                 return
 
             if not callback_tasks:
+                # there are no callbacks to run
                 return
 
+            # run the callbacks as a group
             callback_group = group(task.s(result) for task in callback_tasks)
             callback_group.apply_async()
 
+        # register the task with the app
         task = self.app.task(decorated_function, name=function.__name__)
+
+        # add the task to the list of tasks
         self.tasks[task.name] = task
 
+        if queue:
+            # make sure the queue has been declared in the worker config
+            if queue not in self.get_queue_names():
+                raise ValueError(
+                    f"Queue {queue} is not declared in the worker config"
+                )
+
+            # configure the app to route the task to the queue
+            self.app.conf.task_routes[task.name] = {"queue": queue}
+
         if schedule:
+            # configure the app to schedule the task
             self.app.add_periodic_task(schedule, task)
---
+++ src/acoupi/programs/workers.py
@@ -0,0 +1,67 @@
+"""Acoupi Workers Module.
+
+This module provides the functionality for managing and configuring
+AcoupiWorkers, which are essential components of the Acoupi framework for
+building smart acoustic sensors. AcoupiWorkers are responsible for executing
+tasks related to acoustic sensing and analysis. Each Acoupi program relies on a
+set of AcoupiWorkers, each with a unique name and configuration.
+
+The AcoupiWorker is implemented as a Celery worker, which listens to designated
+queues for task execution and coordination.
+
+"""
+from typing import List, Optional
+
+from pydantic import BaseModel, Field, field_validator
+
+__all__ = [
+    "AcoupiWorker",
+]
+
+
+class AcoupiWorker(BaseModel):
+    """AcoupiWorker Class.
+
+    Represents an individual worker instance within the Acoupi framework. It is
+    responsible for executing tasks related to acoustic sensing and analysis.
+    Each worker is implemented as a Celery worker and listens to designated
+    queues for task execution and coordination.
+    """
+
+    name: str
+    """Name of the worker. Should be unique among different workers."""
+
+    queues: List[str] = Field(default_factory=list)
+    """Queues the worker should listen to. 
+
+    If empty, the worker will listen to all queues."""
+
+    concurrency: Optional[int] = None
+    """Number of concurrent tasks the worker should run.
+
+    If None, the worker will run as many tasks as possible.
+
+    This setting should be set to 1 if the worker if at most
+    one task can be run at a time. This is useful for tasks
+    that are not thread safe or that require use of a resource
+    that can only be used by one task at a time.
+    """
+
+
+class WorkerConfig(BaseModel):
+    """WorkerConfig Class.
+
+    A configuration class used to define the set of workers to be used for an
+    Acoupi program. It allows you to specify the details of each worker, such
+    as its name, concurrency level, and the queues it listens to.
+    """
+
+    workers: List[AcoupiWorker] = Field(default_factory=list)
+
+    @field_validator("workers")
+    def unique_worker_names(cls, workers: List[AcoupiWorker]):
+        """Validate that all workers have unique names."""
+        names = [worker.name for worker in workers]
+        if len(set(names)) != len(names):
+            raise ValueError("Worker names must be unique.")
+        return workers
