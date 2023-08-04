53c96b00edb48dc477cd5c8ffc286dd2b95fb0ee
Santiago Martinez <santiago.mbal@gmail.com>
2023-08-04 22:15:34 +0100

    Update system.__init__.py

--- src/acoupi/system/__init__.py
+++ src/acoupi/system/__init__.py
@@ -6,13 +6,17 @@ such as loading programs and getting celery apps from programs.
 
 from acoupi.system.apps import get_celery_app
 from acoupi.system.configs import is_configured, write_config
-from acoupi.system.constants import ACOUPI_HOME, CONFIG_PATH, PROGRAM_PATH
+from acoupi.system.constants import ACOUPI_HOME, PROGRAM_CONFIG_FILE, PROGRAM_PATH
 from acoupi.system.deployments import get_current_deployment
-from acoupi.system.programs import load_program, write_program_file
+from acoupi.system.programs import (
+    load_program,
+    setup_program,
+    write_program_file,
+)
 
 __all__ = [
     "ACOUPI_HOME",
-    "CONFIG_PATH",
+    "PROGRAM_CONFIG_FILE",
     "PROGRAM_PATH",
     "get_celery_app",
     "get_current_deployment",
@@ -20,4 +24,5 @@ __all__ = [
     "load_program",
     "write_config",
     "write_program_file",
+    "setup_program",
 ]
