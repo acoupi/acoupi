eff0f607aa0541d19f6b88d114c5111c3c12098a
Santiago Martinez <santiago.mbal@gmail.com>
2023-11-21 16:08:42 +0000

    Updated the CLI to for better error handling

--- src/acoupi/cli.py
+++ src/acoupi/cli.py
@@ -2,6 +2,7 @@
 import click
 
 from acoupi import system
+from acoupi.system import exceptions
 
 __all__ = [
     "acoupi",
@@ -15,8 +16,8 @@ __all__ = [
 def acoupi():
     """Welcome to acoupi.
 
-    This is the main command line interface for acoupi
-    and allows you to setup and run acoupi programs.
+    This is the main command line interface for acoupi and allows you to
+    setup and run acoupi programs.
 
     To get started run `acoupi setup` to setup your first program.
     """
@@ -34,6 +35,13 @@ def setup(program: str, args: list[str]):
     """Setup acoupi."""
     try:
         system.setup_program(program, args, prompt=True)
+
+    except exceptions.ProgramNotFoundError as err:
+        # TODO: Improve this messages
+        click.echo(f"program {err.program} not found")
+
+    except exceptions.InvalidProgramError:
+        click.echo("program is invalid")
 
     except ValueError:
         click.echo("program not found")
@@ -55,6 +63,30 @@ def stop():
     """Stop acoupi."""
     system.stop_services()
     system.disable_services()
+
+
+@acoupi.group()
+def config():
+    """Manage acoupi configuration."""
+    if not system.is_configured():
+        click.echo("Acoupi is not setup. Run `acoupi setup` first.")
+        return
+
+
+@config.command()
+def show():
+    """Show acoupi all of the configuration."""
+    # TODO: implement this
+
+
+@config.command()
+def get():
+    """Print a configuration value."""
+
+
+@config.command()
+def set():
+    """Set a configuration value."""
 
 
 if __name__ == "__main__":
