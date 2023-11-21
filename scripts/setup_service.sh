3401ed22b119ac7ba0a45637a5ea3cc0f8681c13
Aude <47717187+audevuilli@users.noreply.github.com>
2023-11-21 17:13:20 +0000

    remove scripts folder installation via .sh files

--- scripts/setup_service.sh
+++
@@ -1,5 +0,0 @@
-sudo ln -sf $HOME/acoupi/src/acoupi/services/acoupi_testing.service /usr/lib/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable acoupi_testing.service
-sudo systemctl start acoupi_testing.service
-systemctl status acoupi_testing.service
