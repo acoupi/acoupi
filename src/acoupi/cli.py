aa9b7ddc8c965ff8bd6f05579b37d99859302ed1
Aude <47717187+audevuilli@users.noreply.github.com>
2023-11-23 14:28:20 +0000

    update cli.py - add config.command() with subcommand get(), sub(), show().

--- src/acoupi/cli.py
+++ src/acoupi/cli.py
@@ -65,6 +65,26 @@ def stop():
     system.disable_services()
 
 
+@acoupi.command()
+def status():
+    """Check the status of acoupi services."""
+    if not system.is_configured():
+        click.echo("Acoupi is not setup. Run `acoupi setup` first.")
+        return
+
+    try:
+        # Check if acoupi services are enabled. 
+        if services.enable_services():
+            click.echo("Acoupi services are ON.")
+        
+        # Check if acoupi services are disabled. 
+        elif services.disable_services():
+            click.echo("Acoupi services are OFF.")
+
+    except Exception as e:
+        click.echo(f"Error checking acoupi status: {e}")
+
+
 @acoupi.group()
 def config():
     """Manage acoupi configuration."""
@@ -75,18 +95,66 @@ def config():
 
 @config.command()
 def show():
-    """Show acoupi all of the configuration."""
-    # TODO: implement this
+    """Show the entire configuration of acoupi."""
+    if not system.is_configured():
+        click.echo("Acoupi is not setup. Run `acoupi setup` first.")
+        return
+
+    try: 
+        config = system.load_config(system.PROGRAM_CONFIG_FILE, system.CeleryConfig)
+        click.echo("Current acoupi Configuration:")
+        click.echo(config.json(inedent=2))
+    
+    except Exception as e:
+        click.echo(f"Error loading configuration: {e}")
 
 
 @config.command()
-def get():
+@click.argument("config_param_name")
+def get(config_param_name: str):
     """Print a configuration value."""
+    if not system.is_configured():
+        click.echo("Acoupi is not setup. Run `acoupi setup` first.")
+        return
+    
+    try: 
+        config = system.load_config(system.PROGRAM_CONFIG_FILE, system.CeleryConfig)
+
+        # Check if the parameter exists in the configuration
+        if hasattr(config, config_param_name):
+            config_param_value = getattr(config, config_param_name)
+            click.echo(f"{config_param_name}: {config_param_value}")
+        else: 
+            click.echo(f"Configuration parameter '{config_param_name}' not found.")
+        
+    except Exception as e:
+        click.echo(f"Error loading configuration: {e}")
 
 
 @config.command()
-def set():
-    """Set a configuration value."""
+@click.argument("config_param_name")
+@click.argument("config_param_value")
+def sub(config_param_name, config_param_value):
+    """Substitute a configuration value."""
+    if not system.is_configured():
+        click.echo("Acoupi is not setup. Run `acoupi setup` first.")
+        return
+    
+    try:
+        config = system.load_config(system.PROGRAM_CONFIG_FILE, system.CeleryConfig)
+
+        # Check if the parameter exists in the configuration
+        if hasattr(config, config_param_name):
+            # Update the configuration with the new value
+            setattr(config, config_param_name, config_param_value)
+            # Write the updated configuration back to the config file
+            system.write_config(config, system.PROGRAM_CONFIG_FILE)
+            click.echo(f"Configuration parameter '{config_param_name}' set to '{config_param_value}'.")
+        else: 
+            click.echo(f"Configuration parameter '{config_param_name}' not found.")
+
+    except Exception as e:
+        click.echo(f"Error loading or updating configuration: {e}")
 
 
 if __name__ == "__main__":
