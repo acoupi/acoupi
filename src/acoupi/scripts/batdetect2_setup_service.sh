dc6fed455663dd156b0fe804342f28269d82de5a
Aude <47717187+audevuilli@users.noreply.github.com>
2023-11-23 20:54:52 +0000

    remove scripts folder - .sh script to setup systemd hearbeat and batdetect2. - no more in used wtih celery.

--- src/acoupi/scripts/batdetect2_setup_service.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-sudo ln -sf $HOME/acoupi/src/acoupi/services/run_batdetect2.service /usr/lib/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable run_batdetect2.service
-sudo systemctl start run_batdetect2.service
-systemctl status run_batdetect2.service
--- src/acoupi/scripts/setup_heartbeat.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-sudo ln -sf $HOME/acoupi/src/acoupi/services/heartbeat.service /usr/lib/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable heartbeat.service
-sudo systemctl start heartbeat.service
-systemctl status heartbeat.service
