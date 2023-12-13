37a7d9810818c39e51ecaca71bd77e9bc821ec23
Santiago Martinez <santiago.mbal@gmail.com>
2023-12-13 15:27:30 +0000

    Marked recording test to skip if no input devices are available

--- tests/test_components/test_audio_recording.py
+++ tests/test_components/test_audio_recording.py
@@ -1,9 +1,16 @@
 """Test the recording of an audio files."""
 from pathlib import Path
 
+import pytest
+
 from acoupi import components, data
+from acoupi.components.audio_recorder import has_input_audio_device
 
 
+@pytest.mark.skipif(
+    not has_input_audio_device(),
+    reason="No audio device found.",
+)
 def test_audio_recording(deployment: data.Deployment, tmp_path: Path):
     """Test the audio file recording."""
     recorder = components.PyAudioRecorder(
