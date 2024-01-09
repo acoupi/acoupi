08a19cd519aae6eaeef9c22437cd31df2b3138bc
Santiago Martinez <santiago.mbal@gmail.com>
2024-01-09 22:32:37 +0000

    fix: removed the second get_microphone_info function inside the record method of the PyAudioRecorder

--- src/acoupi/components/audio_recorder.py
+++ src/acoupi/components/audio_recorder.py
@@ -46,6 +46,7 @@ def has_input_audio_device() -> bool:
 
 def get_microphone_info() -> Tuple[int, int, int]:
     """Check if there are any input audio devices available.
+
     And get the information of a compatible audio device.
 
     Parameters
@@ -61,8 +62,12 @@ def get_microphone_info() -> Tuple[int, int, int]:
 
     Returns
     -------
-    int
-        The samplerate, audio_channel, device_index of the audio device.
+    channels: int
+        The number of audio channels of the audio device.
+    sample_rate: int
+        The samplerate of the audio device.
+    input_device_index: int
+        The input index of the audio device.
 
     Raises
     ------
@@ -155,12 +160,6 @@ class PyAudioRecorder(AudioRecorder):
             # Create an new instace of PyAudio
             p = pyaudio.PyAudio()
 
-            (
-                self.audio_channels,
-                self.samplerate,
-                self.device_index,
-            ) = get_microphone_info()
-
             # Create new audio stream
             stream = p.open(
                 format=pyaudio.paInt16,
@@ -205,5 +204,7 @@ class PyAudioRecorder(AudioRecorder):
                     datetime=self.datetime,
                     duration=self.duration,
                     samplerate=self.samplerate,
+                    audio_channels=self.audio_channels,
+                    chunksize=self.chunksize,
                     deployment=deployment,
                 )
