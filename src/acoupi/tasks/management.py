666482d1ba75912ba9ff0aae5a4725a9b8576464
audevuilli <aude.vuilli@gmail.com>
2024-04-12 18:21:03 +0100

    changed pool type to eventlet

--- src/acoupi/tasks/management.py
+++ src/acoupi/tasks/management.py
@@ -2,10 +2,26 @@ import logging
 from typing import Callable, List, Optional
 
 from acoupi.components import types
+from acoupi import data
 from acoupi.files import delete_recording, get_temp_files
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
+def has_model_output(outputs: List[data.ModelOutput], model_name: str) -> bool:
+    for output in outputs:
+        if output.name_model == model_name:
+            return True
+    return False
+
+
+def has_outputs_from_all_models(outputs: List[data.ModelOutput], model_names: List[str]) -> bool:
+    for model_name in model_names:
+        if not has_model_output(outputs, model_name):
+            return False
+
+    return True
 
 
 def generate_file_management_task(
@@ -13,6 +29,7 @@ def generate_file_management_task(
     file_manager: types.RecordingSavingManager,
     logger: logging.Logger = logger,
     file_filters: Optional[List[types.RecordingSavingFilter]] = None,
+    required_models: Optional[List[str]] = None,
 ) -> Callable[[], None]:
     """Build a process to manage files.
 
@@ -25,9 +42,13 @@ def generate_file_management_task(
     the disk, and remove recordings that are no longer needed.
     """
 
+    if required_models is None:
+        required_models = []
+
     def file_management_task() -> None:
         """Manage files."""
         temp_wav_files = get_temp_files()
+
         recordings_and_outputs = store.get_recordings_by_path(
             paths=temp_wav_files
         )
@@ -41,6 +62,9 @@ def generate_file_management_task(
                     "Temporary recording %s has no path",
                     recording.id,
                 )
+                continue
+
+            if not has_outputs_from_all_models(model_outputs, required_models):
                 continue
 
             if not all(
