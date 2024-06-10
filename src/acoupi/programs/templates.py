166426b127c80cd95273338b613088136ea5d4e2
mbsantiago <santiago.mbal@gmail.com>
2024-06-10 16:38:49 +0100

    Creating a program.templates module to ease program creation

---
+++ src/acoupi/programs/templates.py
@@ -0,0 +1,61 @@
+"""Program templates.
+
+This module provides functions that facilitate the creation of programs.
+"""
+
+import datetime
+from pathlib import Path
+from typing import Annotated, List, Type
+
+from pydantic import BaseModel, Field
+
+from acoupi.data import TimeInterval
+from acoupi.programs.base import NoUserPrompt, AcoupiProgram
+
+__all__ = [
+    "create_base_program",
+]
+
+
+class AudioConfiguration(BaseModel):
+    duration: int = 3
+    """Duration of each audio recording in seconds."""
+
+    interval: int = 5
+    """Interval between each audio recording in seconds."""
+
+    chunksize: Annotated[int, NoUserPrompt] = 8192
+    """Chunksize of audio recording."""
+
+    schedule: List[TimeInterval] = Field(
+        default_factory=lambda: [
+            TimeInterval(start=datetime.time.min, end=datetime.time.max)
+        ]
+    )
+
+
+class FileConfiguration(BaseModel):
+    audio_directory: Path = Field(
+        default_factory=lambda: Path.home() / "audio"
+    )
+    """Directory to save audio recordings."""
+
+
+
+class StorageConfiguration(BaseModel):
+    metadata_db: Path = Field(
+        default_factory=lambda: Path.home() / "metadata.db"
+    )
+
+    messages_db: Path = Field(
+        default_factory=lambda: Path.home() / "messages.db"
+    )
+
+
+def create_base_program(
+    config_schema: Type[BaseModel],
+) -> Type[AcoupiProgram]:
+    class Program(AcoupiProgram):
+        config: config_schema
+
+    return Program
