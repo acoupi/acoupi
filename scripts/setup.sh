56bc26ee6ea865ea68c0c2e4d6df260f97f99d7a
mbsantiago <santiago.mbal@gmail.com>
2024-06-10 17:12:42 +0100

    Skip enable-linger if CI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,15 @@
 
 sudo apt update
 sudo apt install -y \
-  alsa-utils \
-  libasound2-dev \
-  libffi-dev \
-  python3-pyaudio \
-  portaudio19-dev \
-  rabbitmq-server
+    alsa-utils \
+    libasound2-dev \
+    libffi-dev \
+    python3-pyaudio \
+    portaudio19-dev \
+    rabbitmq-server
 
-sudo loginctl enable-linger 
+# Check if CI or GITHUB_RUN_ID are not set (i.e. running locally)
+# and if so run sudo loginctl enable-linger
+if [ -z "$CI" ] && [ -z "$GITHUB_RUN_ID" ]; then
+    sudo loginctl enable-linger
+fi
