3880a45474755c4750bdaf299585ec7855e43b7a
mbsantiago <santiago.mbal@gmail.com>
2024-08-10 22:32:57 +0100

    Minor edits to callback tests

--- tests/test_programs/conftest.py
+++ tests/test_programs/conftest.py
@@ -6,3 +6,17 @@ from acoupi.system.constants import CeleryConfig
 @pytest.fixture(scope="session")
 def celery_config():
     return CeleryConfig().model_dump()
+
+
+@pytest.fixture(scope="session")
+def celery_worker_parameters():
+    """Redefine this fixture to change the init parameters of Celery workers.
+
+    This can be used e. g. to define queues the worker will consume tasks from.
+
+    The dict returned by your fixture will then be used
+    as parameters when instantiating :class:`~celery.worker.WorkController`.
+    """
+    return {
+        "loglevel": "WARN",
+    }
