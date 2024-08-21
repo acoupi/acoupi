9e766a2b5027c7163ff3cc6b66264d9033e0ec1c
audevuilli <aude.vuilli@gmail.com>
2024-08-21 17:30:45 +0100

    run rye formatter

--- tests/test_system/test_lifecycle.py
+++ tests/test_system/test_lifecycle.py
@@ -17,9 +17,7 @@ def test_setup_program_creates_all_the_required_files(
 
     # The program name is stored in a file
     assert settings.program_name_file.exists()
-    assert (
-        settings.program_name_file.read_text() == "acoupi.programs.test"
-    )
+    assert settings.program_name_file.read_text() == "acoupi.programs.test"
 
     # Check that configurations are created
     assert settings.program_config_file.exists()
