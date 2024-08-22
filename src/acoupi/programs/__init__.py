80f75573ae4519ef0fe277ed11a284c8b5f2264a
audevuilli <aude.vuilli@gmail.com>
2024-08-22 13:29:09 +0100

    run rye formatter

--- src/acoupi/programs/__init__.py
+++ src/acoupi/programs/__init__.py
@@ -1,4 +1,8 @@
-from acoupi.programs.custom.base import AcoupiProgram, NoUserPrompt, ProgramConfig
+from acoupi.programs.custom.base import (
+    AcoupiProgram,
+    NoUserPrompt,
+    ProgramConfig,
+)
 from acoupi.programs.custom.workers import AcoupiWorker, WorkerConfig
 
 __all__ = [
