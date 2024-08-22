d75eba4b996c9e76a649bad57f3a94d3f3efc463
audevuilli <aude.vuilli@gmail.com>
2024-08-22 13:18:04 +0100

    add __init__ file to folder programs.

---
+++ src/acoupi/programs/__init__.py
@@ -0,0 +1,10 @@
+from acoupi.programs.custom.base import AcoupiProgram, NoUserPrompt, ProgramConfig
+from acoupi.programs.custom.workers import AcoupiWorker, WorkerConfig
+
+__all__ = [
+    "AcoupiProgram",
+    "AcoupiWorker",
+    "WorkerConfig",
+    "ProgramConfig",
+    "NoUserPrompt",
+]
