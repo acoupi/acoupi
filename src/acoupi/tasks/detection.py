8274d6755e4677572fb238bf4acfc97921139230
audevuilli <aude.vuilli@gmail.com>
2024-08-27 09:35:55 +0100

    fix rebase conflict

--- src/acoupi/tasks/detection.py
+++ src/acoupi/tasks/detection.py
@@ -56,17 +56,8 @@ def generate_detection_task(
 
         # Create messages
         for message_factory in message_factories or []:
-            # Check if there are any tags in the model output
-            has_valid_tags = any(
-                tag
-                for detection in model_output.detections
-                for tag in detection.tags
-            )
-            if has_valid_tags:
-                message = message_factory.build_message(model_output)
-                logger.info("Storing message.")
-                message_store.store_message(message)
-            else:
-                logger.info("No valid tags found.")
+            message = message_factory.build_message(model_output)
+            logger.info("Storing message.")
+            message_store.store_message(message)
 
     return detection_task
