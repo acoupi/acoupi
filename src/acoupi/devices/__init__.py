4f071b30f7c314152f021937875617f596f5dc8e
mbsantiago <santiago.mbal@gmail.com>
2024-10-01 12:54:54 +0100

    Create get_device_id function

--- src/acoupi/devices/__init__.py
+++ src/acoupi/devices/__init__.py
@@ -1,7 +1,18 @@
+import uuid
+
 from acoupi.devices.rpi import get_rpi_host_name, get_rpi_serial_number, is_rpi
+
+
+def get_device_id() -> str:
+    if is_rpi():
+        return get_rpi_serial_number()
+
+    return str(uuid.getnode())
+
 
 __all__ = [
     "get_rpi_serial_number",
     "get_rpi_host_name",
+    "get_device_id",
     "is_rpi",
 ]
