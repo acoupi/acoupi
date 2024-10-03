928f209bb725edbccd52eea8121cd1ba0deac22c
mbsantiago <santiago.mbal@gmail.com>
2024-10-03 09:58:03 +0100

    add logging to file management task

--- src/acoupi/tasks/management.py
+++ src/acoupi/tasks/management.py
@@ -101,6 +101,7 @@ def generate_file_management_task(
                 )
                 continue
 
+            logging.info("Requiring models: %s", required)
             # Is the recording ready to be managed?
             if required - {model.name_model for model in model_outputs}:
                 logger.info(
