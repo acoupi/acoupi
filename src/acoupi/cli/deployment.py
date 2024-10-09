2a19a3e8ce26190e3b01c7b5b337f4cdffb9215a
mbsantiago <santiago.mbal@gmail.com>
2024-10-09 16:49:28 +0100

    Added deployment info to the status command

--- src/acoupi/cli/deployment.py
+++ src/acoupi/cli/deployment.py
@@ -7,7 +7,7 @@ import click
 from acoupi import system
 from acoupi.cli.base import acoupi
 from acoupi.cli.base import check as check_command
-from acoupi.system.celery import CeleryState
+from acoupi.system.celery import CeleryState, WorkerState
 from acoupi.system.programs import ProgramState
 from acoupi.system.services import ServiceStatus
 from acoupi.system.state import AcoupiStatus
@@ -82,6 +82,7 @@ def status(ctx):
     _print_services_status(status)
     _print_celery_status(status)
     _print_program_status(status)
+    _print_deployment_status(status)
 
 
 class Style(TypedDict):
@@ -105,23 +106,29 @@ def _print_services_status(status: AcoupiStatus):
 
     _print_header("System Services", overall_status)
 
-    style = _SERVICE_STATUS_STYLES.get(status.services.acoupi, DEFAULT_STYLE)
+    style = _SERVICE_STATUS_STYLES.get(status.services.acoupi, DEFAULT)
     _print_field("acoupi", status.services.acoupi.value, style)
 
-    style = _SERVICE_STATUS_STYLES.get(status.services.beat, DEFAULT_STYLE)
+    style = _SERVICE_STATUS_STYLES.get(status.services.beat, DEFAULT)
     _print_field("beat", status.services.beat.value, style)
 
 
 def _print_celery_status(status: AcoupiStatus):
     _print_header("Celery", "ok")
 
-    style = _CELERY_STATUS_STYLES.get(status.celery.state, DEFAULT_STYLE)
+    style = _CELERY_STATUS_STYLES.get(status.celery.state, DEFAULT)
     _print_field("status", status.celery.state.value, style)
 
     if not status.celery.workers:
         return
 
-    click.echo("  Workers:")
+    click.echo("")
+    click.secho(f"  {'workers':>10}", bold=True)
+
+    for worker_status in status.celery.workers:
+        style = _WORKER_STATUS_STYLES.get(worker_status.state, DEFAULT)
+        name = worker_status.worker_name.split("@")[0]
+        _print_field(name, worker_status.state.value, style)
 
 
 def _print_program_status(status: AcoupiStatus):
@@ -132,12 +139,42 @@ def _print_program_status(status: AcoupiStatus):
         overall_status = "error"
     _print_header("Program", overall_status)
 
-    style = _PROGRAM_STATUS_STYLES.get(status.program, DEFAULT_STYLE)
+    style = _PROGRAM_STATUS_STYLES.get(status.program, DEFAULT)
     _print_field("status", status.program.value, style)
 
     if status.program == ProgramState.UNHEALTHY:
         click.echo(f"\n{'':>15}Configuration errors were found")
         click.echo(f"{'':>15}Run `acoupi check` for details.")
+
+
+def _print_deployment_status(status: AcoupiStatus):
+    _print_header("Deployment", "ok" if status.deployment else "warning")
+
+    _print_field(
+        "status",
+        "active" if status.deployment else "inactive",
+        SUCCESS if status.deployment else INACTIVE,
+    )
+
+    if not status.deployment:
+        return
+
+    click.echo("")
+    click.secho(f"  {'info':>10}", bold=True)
+
+    _print_field(
+        "started on",
+        status.deployment.started_on.strftime("%A, %d. %B %Y %I:%M%p"),
+        INFO,
+    )
+
+    _print_field("name", status.deployment.name, INFO)
+
+    if status.deployment.latitude is not None:
+        _print_field("latitude", str(status.deployment.latitude), INFO)
+
+    if status.deployment.longitude is not None:
+        _print_field("longitude", str(status.deployment.longitude), INFO)
 
 
 def _print_field(field_name: str, field_value: str, style: Style) -> None:
@@ -160,40 +197,32 @@ def _print_header(name: str, status: Literal["ok", "warning", "error"]):
     click.echo(click.style("●", fg=fg) + click.style(f" {name}", bold=True))
 
 
-DEFAULT_STYLE: Style = {"fg": "white"}
+DEFAULT: Style = {"fg": "white"}
+SUCCESS: Style = {"fg": "green"}
+INACTIVE: Style = {"fg": "blue"}
+INFO: Style = {"fg": "cyan"}
+WARNING: Style = {"fg": "yellow"}
+ERROR: Style = {"fg": "red"}
 
 _SERVICE_STATUS_STYLES: Dict[ServiceStatus, Style] = {
-    ServiceStatus.ACTIVE: {
-        "fg": "green",
-    },
-    ServiceStatus.INACTIVE: {
-        "fg": "blue",
-    },
-    ServiceStatus.FAILED: {
-        "fg": "red",
-    },
+    ServiceStatus.ACTIVE: SUCCESS,
+    ServiceStatus.INACTIVE: INACTIVE,
+    ServiceStatus.FAILED: ERROR,
 }
 
 _CELERY_STATUS_STYLES: Dict[CeleryState, Style] = {
-    CeleryState.ERROR: {
-        "fg": "red",
-    },
-    CeleryState.AVAILABLE: {
-        "fg": "green",
-    },
-    CeleryState.UNAVAILABLE: {
-        "fg": "yellow",
-    },
+    CeleryState.ERROR: ERROR,
+    CeleryState.AVAILABLE: SUCCESS,
+    CeleryState.UNAVAILABLE: WARNING,
 }
 
 _PROGRAM_STATUS_STYLES: Dict[ProgramState, Style] = {
-    ProgramState.OK: {
-        "fg": "green",
-    },
-    ProgramState.ERROR: {
-        "fg": "red",
-    },
-    ProgramState.UNHEALTHY: {
-        "fg": "red",
-    },
+    ProgramState.OK: SUCCESS,
+    ProgramState.ERROR: ERROR,
+    ProgramState.UNHEALTHY: ERROR,
+}
+
+_WORKER_STATUS_STYLES: Dict[WorkerState, Style] = {
+    WorkerState.OK: SUCCESS,
+    WorkerState.NOTOK: ERROR,
 }
--- src/acoupi/system/state.py
+++ src/acoupi/system/state.py
@@ -1,9 +1,13 @@
 """Functions for accessing the state of the Acoupi system."""
 
+from typing import Optional
+
 from pydantic import BaseModel
 
+from acoupi.data import Deployment
 from acoupi.system.celery import CeleryStatus, get_celery_status
 from acoupi.system.constants import Settings
+from acoupi.system.deployments import get_current_deployment
 from acoupi.system.programs import ProgramState, get_program_state
 from acoupi.system.services import (
     ServiceStatus,
@@ -41,6 +45,7 @@ class AcoupiStatus(BaseModel):
     services: ServicesStatus
     celery: CeleryStatus
     program: ProgramState
+    deployment: Optional[Deployment]
 
 
 def is_configured(settings: Settings) -> bool:
@@ -81,11 +86,17 @@ def get_status(settings: Settings) -> AcoupiStatus:
     beat_service_status = get_acoupi_beat_service_status(settings)
     celery_status = get_celery_status(settings)
     program_state = get_program_state(settings)
+    try:
+        deployment = get_current_deployment(settings)
+    except Exception:
+        deployment = None
+
     return AcoupiStatus(
         services=ServicesStatus(
             acoupi=acoupi_service_status,
             beat=beat_service_status,
         ),
+        deployment=deployment,
         celery=celery_status,
         program=program_state,
     )
